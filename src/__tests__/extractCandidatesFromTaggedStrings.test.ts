cbbc1336e204ec5076ef89757fbf28607c0dce98
Alexandre Borela <alexandre@borela.tech>
2025-05-17 16:19:21 -0300

    Add code to extract candidates from tagged strings

---
+++ src/__tests__/extractCandidatesFromTaggedStrings.test.ts
@@ -0,0 +1,46 @@
+import {extractCandidatesFromTaggedStrings} from '../extractCandidatesFromTaggedStrings'
+import {trimLines} from './utils/trimLines'
+
+describe('extractCandidatesFromTaggedStrings()', () => {
+  it('extracts candidates from tagged strings', () => {
+    const input = `
+      const SOME_CSS = tailwind\`
+        absolute
+        !bg-[
+          linear-gradient(
+            to_right,
+            theme(colors.zinc.900/50%),
+            transparent
+          ),
+          linear-gradient(
+            to_right,
+            theme(colors.purple.600/70%),
+            theme(colors.purple.800/20%)_32px,
+            transparent_50%
+          )
+        ]
+        !border-purple-600
+      \`
+    `
+
+    const transformedCode = trimLines`const SOME_CSS = \`absolute
+      !bg-[linear-gradient(to_right,theme(colors.zinc.900/50%),transparent),linear-gradient(to_right,theme(colors.purple.600/70%),theme(colors.purple.800/20%)_32px,transparent_50%)]
+      !border-purple-600\`;
+    `
+
+    const expected = {
+      candidatesFound: [
+        'absolute',
+        '!bg-[linear-gradient(to_right,theme(colors.zinc.900/50%),transparent),linear-gradient(to_right,theme(colors.purple.600/70%),theme(colors.purple.800/20%)_32px,transparent_50%)]',
+        '!border-purple-600',
+      ],
+      transformedCode: {
+        code: transformedCode,
+        map: null,
+      },
+    }
+
+    const result = extractCandidatesFromTaggedStrings(input)
+    expect(result).toEqual(expected)
+  })
+})
---
+++ src/extractCandidatesFromTaggedStrings.ts
@@ -0,0 +1,69 @@
+import * as t from '@babel/types'
+import generate from '@babel/generator'
+import traverse from '@babel/traverse'
+import {parse} from '@babel/parser'
+import {squishMultilineExpressions} from './squishMultilineExpressions'
+
+export function extractCandidatesFromTaggedStrings(code: string) {
+  const candidatesFound: string[] = []
+  const ast = parse(code, {
+    sourceType: 'module',
+    plugins: [
+      'jsx',
+      'typescript',
+    ],
+  })
+
+  traverse(ast, {
+    TaggedTemplateExpression(path) {
+      const {
+        tag,
+        quasi: {
+          quasis,
+        },
+      } = path.node
+
+      if (!t.isIdentifier(tag))
+        return
+
+      if (tag.name !== 'tailwind')
+        return
+
+      if (quasis.length !== 1)
+        throw new Error('Tailwind tagged template should have exactly one argument.')
+
+      const rawString = quasis[0].value.raw
+      const squished = squishMultilineExpressions(rawString)
+      const filtered = squished
+        .split('\n')
+        .filter(Boolean)
+
+      candidatesFound.push(...filtered)
+
+      path.replaceWith(
+        t.templateLiteral(
+          [
+            t.templateElement({
+              raw: squished,
+              cooked: squished,
+            }),
+          ],
+          [],
+        ),
+      )
+    },
+  })
+
+  const {
+    code: transformedCode,
+    map: transformedCodeMap,
+  } = generate(ast, {}, code)
+
+  return {
+    candidatesFound,
+    transformedCode: {
+      code: transformedCode,
+      map: transformedCodeMap,
+    },
+  }
+}
