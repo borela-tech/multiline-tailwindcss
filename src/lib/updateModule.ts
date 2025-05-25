42c9a48f75108c7a887f56170537aedad665991e
Alexandre Borela <alexandre@borela.tech>
2025-05-25 14:30:58 -0300

    Use intervals to force css update

--- src/lib/updateModule.ts
+++ src/lib/updateModule.ts
@@ -1,13 +1,31 @@
 import {ViteDevServer} from 'vite'
 
+const intervals: Map<string, NodeJS.Timeout> = new Map()
+
 export function updateModule(
   devServer: ViteDevServer,
   id: string,
 ) {
-  const module = devServer.moduleGraph.getModuleById(id)
+  let module = devServer.moduleGraph.getModuleById(id)
   if (!module)
     return
 
+  let interval = intervals.get(id)
+  if (interval)
+    clearInterval(interval)
+
   devServer.moduleGraph.invalidateModule(module)
-  devServer.reloadModule(module)
+
+  interval = setInterval(() => {
+    if (module.invalidationState != 'HARD_INVALIDATED') {
+      clearInterval(interval)
+      intervals.delete(id)
+      return
+    }
+
+    devServer.moduleGraph.invalidateModule(module)
+    devServer.reloadModule(module)
+  }, 10)
+
+  intervals.set(id, interval)
 }
