17354b80f32951de3f4bb8f5fe96ee9765447bb0
Alexandre Borela <alexandre@borela.tech>
2025-05-30 18:24:12 -0300

    Fix typo

--- src/plugins/getConfigOptionsPlugin.ts
+++ src/plugins/getConfigOptionsPlugin.ts
@@ -3,7 +3,7 @@ import {SharedState} from './SharedState'
 
 export function getConfigOptionsPlugin(state: SharedState): Plugin {
   return {
-    name: '@borela-tech/vite-plugin-multiline-tailwind:get-config=options',
+    name: '@borela-tech/vite-plugin-multiline-tailwind:get-config-options',
     enforce: 'pre',
     configResolved(config) {
       state.projectRoot = config.root
