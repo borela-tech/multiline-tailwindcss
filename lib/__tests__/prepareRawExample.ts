24d56362ff74688a91333139d40f0a835681097f
Alexandre Borela <alexandre@borela.tech>
2025-09-28 12:56:12 -0300

    Add raw examples

---
+++ lib/__tests__/prepareRawExample.ts
@@ -0,0 +1,9 @@
+export function prepareRawExample(text: string): string[] {
+  const blocks = text.split(/\n\s*\n/)
+  const trimmedBlocks = blocks.map(
+    block => block.split('\n')
+      .map(line => line.trim())
+      .join(' '),
+  )
+  return trimmedBlocks.filter(block => block.length > 0)
+}
