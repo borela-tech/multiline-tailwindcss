f838ba68652adb99c6f5aa3806f82fb7f84f1246
Alexandre Borela <alexandre@borela.tech>
2025-12-07 13:31:30 -0300

    Adjust quoted string parsing

--- lib/parser/parseQuotedString.ts
+++ lib/parser/parseQuotedString.ts
@@ -1,24 +1,40 @@
 import {next} from './next'
+import {peek} from './peek'
 import {QuotedStringNode} from './QuotedStringNode'
 import {State} from './State'
 
 export function parseQuotedString(state: State): QuotedStringNode {
   const quote = next(state)
+  let closed = false
   let value = ''
 
   while (state.pos < state.input.length) {
     const ch = next(state)
 
     if (ch === '\\') {
-      value += next(state)
+      // Remove escape from quotes used in the string.
+      if (peek(state) === quote) {
+        value += next(state)
+        continue
+      }
+
+      value += ch + next(state)
       continue
     }
 
-    if (ch === quote)
+    if (ch === quote) {
+      closed = true
       break
+    }
 
-    value += ch
+    if (ch === ' ')
+      value += '_'
+    else
+      value += ch
   }
+
+  if (!closed)
+    throw new Error(`Unclosed quoted string starting at position ${state.pos}`)
 
   return {
     type: 'QuotedString',
