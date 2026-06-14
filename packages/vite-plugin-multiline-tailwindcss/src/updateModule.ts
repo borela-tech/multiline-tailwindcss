d872395f50b7a1e50ad6c46328eeb3310e2d2096
Alexandre Borela <alexandre@borela.tech>
2026-06-14 11:36:33 -0300

    Use module URL to trigger update

--- packages/vite-plugin-multiline-tailwindcss/src/updateModule.ts
+++ packages/vite-plugin-multiline-tailwindcss/src/updateModule.ts
@@ -1,9 +1,13 @@
 import type {ViteDevServer} from 'vite'
 
 export function updateModule(devServer: ViteDevServer, id: string) {
+  const module = devServer.moduleGraph.getModuleById(id)
+  if (!module)
+    return
+  devServer.moduleGraph.invalidateModule(module)
   devServer.hot.send('vite:invalidate', {
     firstInvalidatedBy: '@borela-tech/vite-plugin-multiline-tailwindcss',
     message: 'New Tailwind candidates found.',
-    path: id,
+    path: module.url,
   })
 }
